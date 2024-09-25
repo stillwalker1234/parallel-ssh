cc429219dbc7ca759eb0dff40bbc5b4a5267bc93
stillwalker1234 <mail@stillwalker.dk>
2024-09-25 15:10:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,14 +15,8 @@
 
 from setuptools import setup, find_packages
 
-import versioneer
-
-
-cmdclass = versioneer.get_cmdclass()
-
 setup(name='parallel-ssh',
-      version=versioneer.get_version(),
-      cmdclass=cmdclass,
+      version="1",
       description='Asynchronous parallel SSH library',
       long_description=open('README.rst').read(),
       author='Panos Kittenis',
@@ -35,25 +29,5 @@ setup(name='parallel-ssh',
                         '*.tests', '*.tests.*')
       ),
       install_requires=[
-          'gevent', 'ssh2-python', 'ssh-python'],
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
-          'Intended Audience :: Developers',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Topic :: System :: Networking',
-          'Topic :: Software Development :: Libraries',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Operating System :: POSIX :: Linux',
-          'Operating System :: POSIX :: BSD',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: MacOS :: MacOS X',
-      ],
+          'gevent', 'ssh2-python312', 'ssh-python'],
       )
